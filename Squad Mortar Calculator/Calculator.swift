05ee3c4f707f56d737a7d0b40639100d3a4faec6
James Longman <you@example.com>
2018-02-12 00:37:19 +0000

    removing unneeded files

---
+++ Squad Mortar Calculator/Calculator.swift
@@ -0,0 +1,18 @@
+//
+//  Calculator.swift
+//  Squad Mortar Calculator
+//
+//  Created by James Longman on 07/02/2018.
+//  Copyright © 2018 James Longman. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    var mortarField1: String?
+    
+    func mortarField1(input: String) {
+        mortarField1 = input
+        print(mortarField1!)
+    }
+}
