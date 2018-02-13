4afacd69cf48cb93f0a50ca52b8843373ab2adef
James Longman <you@example.com>
2018-02-13 16:28:54 +0000

    Added 'more' tab, will implement user guide here after calculator is set up, also added an about section to be completed

---
+++ Squad Mortar Calculator/More VCs/GuideViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GuideViewController.swift
+//  Squad Mortar Calculator
+//
+//  Created by James Longman on 13/02/2018.
+//  Copyright © 2018 James Longman. All rights reserved.
+//
+
+import UIKit
+
+class GuideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func backButton(_ sender: Any) {
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func backTextButton(_ sender: Any) {
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ Squad Mortar Calculator/More VCs/MoreViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MoreViewController.swift
+//  Squad Mortar Calculator
+//
+//  Created by James Longman on 13/02/2018.
+//  Copyright © 2018 James Longman. All rights reserved.
+//
+
+import UIKit
+
+class MoreViewController: UITableViewController {
+    let moreTable = ["User Guide", "About", "Leave A Review"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return moreTable.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let moreCell = self.tableView.dequeueReusableCell(withIdentifier: "moreCell", for: indexPath)
+        moreCell.textLabel?.text = moreTable[indexPath.row]
+        return moreCell
+        
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (moreTable[indexPath.row] == "User Guide") {
+            let guideView = storyboard!.instantiateViewController(withIdentifier: "GuideView") as! GuideViewController
+            self.present(guideView, animated: true, completion: nil)
+        } else if (moreTable[indexPath.row] == "About") {
+            let aboutView = storyboard!.instantiateViewController(withIdentifier: "AboutView") as! AboutViewController
+            self.present(aboutView, animated: true, completion: nil)
+        } else if (moreTable[indexPath.row] == "Leave A Review") {
+            print("REVIEW LINK")
+        } else {
+            return
+        }
+    }
+}
