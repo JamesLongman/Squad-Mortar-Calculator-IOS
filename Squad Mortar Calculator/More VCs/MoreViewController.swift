c18b911ddd50279dca35f7b23e707e3f7d259f15
James Longman <you@example.com>
2018-02-22 20:02:00 +0000

    Added animation to more tab appearing to improve its presentation in preparation for #10

--- Squad Mortar Calculator/More VCs/MoreViewController.swift
+++ Squad Mortar Calculator/More VCs/MoreViewController.swift
@@ -50,4 +50,27 @@ class MoreViewController: UITableViewController {
             return
         }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        animateTable()
+    }
+    
+    // Simple animation to bring rows into view when more view appears
+    func animateTable() {
+        tableView.reloadData()
+        let cells = tableView.visibleCells
+        let tableViewHeight = tableView.bounds.size.height
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableViewHeight)
+        }
+        
+        var delayCounter = 0
+        for cell in cells {
+            UIView.animate(withDuration: 1.5, delay: Double(delayCounter) * 0.05, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                cell.transform = CGAffineTransform.identity
+                }, completion: nil)
+            delayCounter += 1
+        }
+    }
 }
